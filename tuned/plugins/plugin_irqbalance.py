22924b9f0ce9424527aad2aa5f1764aaea60549b
spyroot <spyroot@gmail.com>
2023-03-10 11:18:30 +0400

    polished fixed bunch of issue related python 3, move all to static it much easy to make sense especially if it concurrent code.

--- tuned/plugins/plugin_irqbalance.py
+++ tuned/plugins/plugin_irqbalance.py
@@ -90,18 +90,18 @@ class IrqbalancePlugin(base.Plugin):
     def _restart_irqbalance(self):
         # Exit code 5 means unit not found (see 'EXIT_NOTINSTALLED' in
         # systemd.exec(5))
-        retcode, out = self._cmd.execute(
+        ret_code, out = self._cmd.execute(
             ["systemctl", "try-restart", "irqbalance"],
             no_errors=[5])
-        if retcode != 0:
+        if ret_code != 0:
             log.warn("Failed to restart irqbalance. Is it installed?")
 
-    def _set_banned_cpus(self, banned_cpumask):
+    def _set_banned_cpus(self, banned_cpu_mask):
         content = self._read_irqbalance_sysconfig()
         if content is None:
             return
         content = self._clear_banned_cpus(content)
-        content = self._write_banned_cpus(content, banned_cpumask)
+        content = self._write_banned_cpus(content, banned_cpu_mask)
         if self._write_irqbalance_sysconfig(content):
             self._restart_irqbalance()
 
@@ -114,24 +114,24 @@ class IrqbalancePlugin(base.Plugin):
             self._restart_irqbalance()
 
     @command_custom("banned_cpus", per_device=False)
-    def _banned_cpus(self, enabling, value, verify):
-        banned_cpumask = None
+    def _banned_cpus(self, enabling, value, verify, **kwargs):
+        banned_cpu_mask = None
         if value is not None:
             banned = set(self._cmd.cpulist_unpack(value))
             present = set(self._cpus)
             if banned.issubset(present):
-                banned_cpumask = self._cmd.cpulist2hex(list(banned))
+                banned_cpu_mask = self._cmd.cpulist2hex(list(banned))
             else:
                 str_cpus = ",".join([str(x) for x in self._cpus])
                 log.error("Invalid banned_cpus specified, '%s' does not match available cores '%s'"
                           % (value, str_cpus))
 
-        if (enabling or verify) and banned_cpumask is None:
+        if (enabling or verify) and banned_cpu_mask is None:
             return None
         if verify:
             # Verification is currently not supported
             return None
         elif enabling:
-            self._set_banned_cpus(banned_cpumask)
+            self._set_banned_cpus(banned_cpu_mask)
         else:
             self._restore_banned_cpus()
